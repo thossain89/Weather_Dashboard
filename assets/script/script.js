60844d2105ac9b80144885bf277733f1818e234e
thossain <tanvirhossain2006@gmail.com>
2021-07-09 23:50:56 +1000

    Html Content and styles added

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+// Weather DashBoard Homework-6 By Tanvir Hossain
+
+
+var searchButton = $("#searchBtn");
+
+var apiKey = "2dbd23399fe87391bfd33a8aa0188fc3"
+
+// Loop to keep saved cities in a list on HTML
